a1bdb409ee2f98071610a29997ce535f17c65e64
biglanggwapoventures <biglanggwapoventures@gmail.com>
2016-09-29 19:48:30 +0800

    added full db struct and deleted old db files

--- 092916.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `uscert`.`organizations` 
-CHANGE COLUMN `name` `name` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL COMMENT '' ;
--- uscert-full-092916.sql
+++ uscert-full-092916.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 16, 2016 at 09:07 AM
+-- Generation Time: Sep 29, 2016 at 07:47 PM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -31,23 +31,53 @@ CREATE TABLE IF NOT EXISTS `attendance` (
   `user_id` int(10) unsigned NOT NULL,
   `datetime_in` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `datetime_out` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `attendance`
+-- Table structure for table `incident_reports`
 --
 
-INSERT INTO `attendance` (`id`, `user_id`, `datetime_in`, `datetime_out`) VALUES
-(15, 5, '2016-07-24 00:01:00', '2016-07-24 14:50:50'),
-(16, 4, '2016-07-24 14:44:45', '2016-07-24 14:50:35'),
-(17, 6, '2016-07-24 14:50:52', '2016-07-24 14:55:23'),
-(18, 5, '2016-07-28 01:18:22', '0000-00-00 00:00:00'),
-(19, 4, '2016-08-08 06:56:59', '2016-08-08 06:58:16'),
-(20, 5, '2016-08-08 06:58:18', '2016-08-08 14:00:00'),
-(21, 5, '2016-08-12 01:02:00', '2016-08-12 05:00:00'),
-(22, 5, '2016-08-14 00:18:00', '2016-08-14 15:54:19'),
-(23, 4, '2016-08-14 13:16:02', '2016-08-14 14:03:14'),
-(24, 6, '2016-08-14 15:48:12', '2016-08-14 15:53:25');
+CREATE TABLE IF NOT EXISTS `incident_reports` (
+  `id` int(10) unsigned NOT NULL,
+  `incident_type` enum('FIRE','FLOOD','EARTHQUAKE','CRASH') COLLATE utf8_bin NOT NULL,
+  `incident_date` date NOT NULL DEFAULT '0000-00-00',
+  `actions_taken` text COLLATE utf8_bin NOT NULL,
+  `other_information` text COLLATE utf8_bin NOT NULL,
+  `alarm` varchar(50) COLLATE utf8_bin NOT NULL,
+  `casualty` text COLLATE utf8_bin NOT NULL,
+  `investigator` varchar(100) COLLATE utf8_bin NOT NULL,
+  `vehicles_used` text COLLATE utf8_bin,
+  `structures_involved` text COLLATE utf8_bin,
+  `estimated_damage` float NOT NULL DEFAULT '0',
+  `cause` text COLLATE utf8_bin NOT NULL,
+  `key_details` text COLLATE utf8_bin,
+  `longitude` decimal(9,6) NOT NULL,
+  `latitude` decimal(9,6) NOT NULL,
+  `formatted_address` varchar(100) COLLATE utf8_bin NOT NULL,
+  `zoom` int(10) unsigned NOT NULL DEFAULT '0',
+  `created_by` int(10) unsigned DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `approved_by` int(10) unsigned DEFAULT NULL,
+  `approved_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `rejected_by` int(10) unsigned DEFAULT NULL,
+  `rejected_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `map_markers`
+--
+
+CREATE TABLE IF NOT EXISTS `map_markers` (
+  `id` int(10) unsigned NOT NULL,
+  `latitude` decimal(9,6) NOT NULL,
+  `longitude` decimal(9,6) NOT NULL,
+  `formatted_address` varchar(255) COLLATE utf8_bin NOT NULL,
+  `status` enum('0','1') COLLATE utf8_bin NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -57,18 +87,9 @@ INSERT INTO `attendance` (`id`, `user_id`, `datetime_in`, `datetime_out`) VALUES
 
 CREATE TABLE IF NOT EXISTS `organizations` (
   `id` int(10) unsigned NOT NULL,
-  `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
   `acronym` varchar(20) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `organizations`
---
-
-INSERT INTO `organizations` (`id`, `name`, `acronym`) VALUES
-(4, 'Sample Name', 'SN'),
-(5, 'Sample Name 2', 'SN2'),
-(6, 'Sample Name 3', 'SN3');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -78,24 +99,16 @@ INSERT INTO `organizations` (`id`, `name`, `acronym`) VALUES
 
 CREATE TABLE IF NOT EXISTS `persons` (
   `id` int(10) unsigned NOT NULL,
-  `firstname` varchar(45) COLLATE utf8_bin NOT NULL,
-  `lastname` varchar(45) COLLATE utf8_bin NOT NULL,
-  `gender` enum('MALE','FEMALE') COLLATE utf8_bin NOT NULL,
-  `birthdate` date NOT NULL DEFAULT '0000-00-00',
-  `nationality` varchar(45) COLLATE utf8_bin NOT NULL,
-  `contact_number` varchar(45) COLLATE utf8_bin NOT NULL,
-  `address` text COLLATE utf8_bin NOT NULL,
+  `firstname` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `lastname` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `gender` enum('MALE','FEMALE') COLLATE utf8_bin DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  `nationality` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `contact_number` varchar(45) COLLATE utf8_bin DEFAULT NULL,
+  `address` text COLLATE utf8_bin,
   `course` varchar(45) COLLATE utf8_bin DEFAULT NULL,
   `skills` text COLLATE utf8_bin
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `persons`
---
-
-INSERT INTO `persons` (`id`, `firstname`, `lastname`, `gender`, `birthdate`, `nationality`, `contact_number`, `address`, `course`, `skills`) VALUES
-(1, 'Adrian', 'Natabio', 'MALE', '1995-06-20', 'Filipino', '', 'mandaue ity', '123', '213'),
-(2, 'Harry', 'Wells', 'MALE', '1962-02-10', 'American', '', 'Seattle, New Jersey', 'BS CpE', 'Hukad kan-on');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -110,21 +123,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `login_type` enum('v','a','sa') COLLATE utf8_bin NOT NULL DEFAULT 'v',
   `person_id` int(10) unsigned DEFAULT NULL,
   `organization_id` int(10) unsigned DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `login_username`, `login_password`, `login_type`, `person_id`, `organization_id`) VALUES
-(1, 'gwapoko1', '6ef5b8e14f85b79ea0c56e78fdd4779e', 'a', NULL, 6),
-(2, 'gwapoko', '8c4205ec33d8f6caeaaaa0c10a14138c', 'a', NULL, 6),
-(4, 'adriannatabio', '5f4dcc3b5aa765d61d8327deb882cf99', 'sa', 1, NULL),
-(5, 'admin', '8c4205ec33d8f6caeaaaa0c10a14138c', 'a', 2, 6),
-(6, 'volun', '9e0b11af3b94614b4508bc6d85d3790f', 'v', NULL, 6),
-(7, 'teer', '9e0b11af3b94614b4508bc6d85d3790f', 'v', NULL, 5),
-(8, 'admin2', 'c84258e9c39059a89ab77d846ddab909', 'a', NULL, 4),
-(9, 'helloworld', '8c4205ec33d8f6caeaaaa0c10a14138c', 'v', NULL, 4);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -135,16 +134,10 @@ INSERT INTO `users` (`id`, `login_username`, `login_password`, `login_type`, `pe
 CREATE TABLE IF NOT EXISTS `vehicles` (
   `id` int(10) unsigned NOT NULL,
   `name` varchar(45) COLLATE utf8_bin NOT NULL,
+  `plate_number` varchar(45) COLLATE utf8_bin NOT NULL,
   `vehicle_type_id` int(10) unsigned NOT NULL,
   `organization_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `vehicles`
---
-
-INSERT INTO `vehicles` (`id`, `name`, `vehicle_type_id`, `organization_id`) VALUES
-(1, 'sadsad', 1, 6);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -155,15 +148,7 @@ INSERT INTO `vehicles` (`id`, `name`, `vehicle_type_id`, `organization_id`) VALU
 CREATE TABLE IF NOT EXISTS `vehicle_types` (
   `id` int(10) unsigned NOT NULL,
   `name` varchar(45) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `vehicle_types`
---
-
-INSERT INTO `vehicle_types` (`id`, `name`) VALUES
-(1, 'Ambulance'),
-(2, 'Firetruck');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Indexes for dumped tables
@@ -175,6 +160,21 @@ INSERT INTO `vehicle_types` (`id`, `name`) VALUES
 ALTER TABLE `attendance`
   ADD PRIMARY KEY (`id`),
   ADD KEY `attendance_user_id_foreign_idx` (`user_id`);
+
+--
+-- Indexes for table `incident_reports`
+--
+ALTER TABLE `incident_reports`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `incident_reports_created_by_foreign_idx` (`created_by`),
+  ADD KEY `incident_reports_approved_by_foreign_idx` (`approved_by`),
+  ADD KEY `incident_reports_rejected_by_foreign_idx` (`rejected_by`);
+
+--
+-- Indexes for table `map_markers`
+--
+ALTER TABLE `map_markers`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `organizations`
@@ -204,6 +204,7 @@ ALTER TABLE `users`
 ALTER TABLE `vehicles`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `name_UNIQUE` (`name`),
+  ADD UNIQUE KEY `plate_number` (`plate_number`),
   ADD KEY `vehicles_vehicle_type_id_foreign_idx` (`vehicle_type_id`),
   ADD KEY `vehicles_organization_id_foreign_idx` (`organization_id`);
 
@@ -222,32 +223,42 @@ ALTER TABLE `vehicle_types`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=25;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `incident_reports`
+--
+ALTER TABLE `incident_reports`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `map_markers`
+--
+ALTER TABLE `map_markers`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `organizations`
 --
 ALTER TABLE `organizations`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `persons`
 --
 ALTER TABLE `persons`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `vehicles`
 --
 ALTER TABLE `vehicles`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `vehicle_types`
 --
 ALTER TABLE `vehicle_types`
-  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
 --
 -- Constraints for dumped tables
 --
@@ -256,7 +267,15 @@ ALTER TABLE `vehicle_types`
 -- Constraints for table `attendance`
 --
 ALTER TABLE `attendance`
-  ADD CONSTRAINT `attendance_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `attendance_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `incident_reports`
+--
+ALTER TABLE `incident_reports`
+  ADD CONSTRAINT `incident_reports_approved_by_foreign` FOREIGN KEY (`approved_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `incident_reports_created_by_foreign` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `incident_reports_rejected_by_foreign` FOREIGN KEY (`rejected_by`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `users`
